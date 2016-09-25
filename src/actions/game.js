444f9eb07df36c000503209e3e7e03886979cd2b
Jeffrey Carl Faden <jeffreyatw@gmail.com>
2016-09-24 18:47:19 -0700

    Clear countdown interval when restarting

--- src/actions/game.js
+++ src/actions/game.js
@@ -1,5 +1,7 @@
 import actionTypes from '../actionTypes';
 
+let countdownInterval;
+
 export const gameOver = () => ({
   type: actionTypes.GAME_OVER,
 });
@@ -8,22 +10,27 @@ export const start = () => ({
   type: actionTypes.START,
 });
 
-export const restart = () => ({
-  type: actionTypes.RESTART,
-});
-
 export const setCountdown = count => ({
   type: actionTypes.SET_COUNTDOWN,
   count,
 });
 
+export const actuallyRestart = () => ({
+  type: actionTypes.RESTART,
+})
+
+export const restart = () => dispatch => {
+  clearInterval(countdownInterval);
+  dispatch(actuallyRestart());
+};
+
 export const beginCountdown = () => dispatch => {
-  let interval;
+  clearInterval(countdownInterval);
   let count = 4;
   const decrement = () => {
     count -= 1;
     if (count === 0) {
-      window.clearInterval(interval);
+      window.clearInterval(countdownInterval);
       dispatch(gameOver());
     } else {
       dispatch(setCountdown(count));
@@ -31,5 +38,5 @@ export const beginCountdown = () => dispatch => {
   }
 
   decrement();
-  interval = window.setInterval(decrement, 1000);
+  countdownInterval = window.setInterval(decrement, 1000);
 }
